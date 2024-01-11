5876724a6b9e6164cf803b6d97415e3a08b82ea7
trust1204@gmail.com <trust1204@gmail.com>
2024-01-12 01:17:20 +0900

    [chore] comment out Repository

--- src/main/java/CoBo/ChatbotChat/Repository/ChatbotChatRepository.java
+++ src/main/java/CoBo/ChatbotChat/Repository/ChatbotChatRepository.java
@@ -1,9 +1,9 @@
-package CoBo.ChatbotChat.Repository;
-
-import CoBo.ChatbotChat.Data.Entity.ChatbotChat;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatbotChatRepository extends JpaRepository<ChatbotChat, Integer> {
-}
+//package CoBo.ChatbotChat.Repository;
+//
+//import CoBo.ChatbotChat.Data.Entity.ChatbotChat;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface ChatbotChatRepository extends JpaRepository<ChatbotChat, Integer> {
+//}
