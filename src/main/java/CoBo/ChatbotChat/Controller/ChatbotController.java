42b9f62135132056b55e9736a1146dae812115bb
trust1204@gmail.com <trust1204@gmail.com>
2024-01-20 12:38:33 +0900

    [feat] save log in database when student use chatbot

--- src/main/java/CoBo/ChatbotChat/Controller/ChatbotController.java
+++ src/main/java/CoBo/ChatbotChat/Controller/ChatbotController.java
@@ -1,9 +1,11 @@
 package CoBo.ChatbotChat.Controller;
 
 import CoBo.ChatbotChat.Service.ChatbotService;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController("/api/chatbot")
@@ -14,7 +16,7 @@ public class ChatbotController {
     private final ChatbotService chatbotService;
 
     @GetMapping("/chat")
-    public ResponseEntity<String> getChat(@RequestParam String question){
-        return chatbotService.getChat(question);
+    public ResponseEntity<String> getChat(@RequestParam String question, @Parameter(hidden = true)Authentication authentication){
+        return chatbotService.getChat(question, authentication);
     }
 }
--- src/main/java/CoBo/ChatbotChat/Repository/ChatbotChatRepository.java
+++ src/main/java/CoBo/ChatbotChat/Repository/ChatbotChatRepository.java
@@ -2,7 +2,8 @@ package CoBo.ChatbotChat.Repository;
 
 import CoBo.ChatbotChat.Data.Entity.ChatbotChat;
 import CoBo.ChatbotChat.Data.Entity.Embedded.ChatbotChatId;
+import CoBo.ChatbotChat.Repository.Custom.ChatbotChatRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatbotChatRepository extends JpaRepository<ChatbotChat, ChatbotChatId> {
+public interface ChatbotChatRepository extends JpaRepository<ChatbotChat, ChatbotChatId>, ChatbotChatRepositoryCustom {
 }
